4e1ee46d654660a2e81c4be2870710329fb53f79
Ciaran <cslemon96@gmail.com>
2025-04-06 14:45:02 +0200

    wip(satellite.cpp): start adding globe

--- satellite.cpp
+++ satellite.cpp
@@ -1,9 +1,6 @@
 #include "player.h"
 #include "raylib.h"
-#include <math.h>
 #include <stdlib.h>
-
-// WINDOW & CONFIG
 
 int W = 800;
 int H = 800;
@@ -12,22 +9,49 @@ float TRACKING_SPEED = 0.025f;
 Vector3 DRIFT_VECTOR = {0.01f, 0.0f, 0.005f};
 Rectangle SCREEN_RECT = {GAP, GAP, W - GAP, H - GAP};
 
-void update(Player *p) {
+typedef struct {
+    Model sphere;
+    float rotation;
+} Globe;
+
+Globe *NewGlobe() {
+    Globe *globe = (Globe *)malloc(sizeof(Globe));
+    globe->rotation = 0.0;
+    globe->sphere = LoadModelFromMesh(GenMeshSphere(10.0, 10, 10));
+    return globe;
+}
+
+void CleanupGlobe(Globe *g) {
+    UnloadModel(g->sphere);
+    free(g);
+}
+
+void UpdateGlobe(Globe *g) {
+    g->rotation += DRIFT_VECTOR.z;
+}
+
+// Game
+
+Player *p;
+Globe *g;
+
+void update() {
     // Update screen size
     if (IsWindowResized()) {
         W = GetScreenWidth();
         H = GetScreenHeight();
         SCREEN_RECT = Rectangle{GAP, GAP, W - GAP, H - GAP};
     }
+    UpdateGlobe(g);
     UpdatePlayer(p);
 }
 
-void draw(Player *p) {
+void draw() {
     BeginDrawing();
     ClearBackground(BLACK);
 
     BeginMode3D(p->cam);
-    DrawSphereWires(Vector3{0}, 10.0f, 10, 10, RAYWHITE);
+    DrawModelWiresEx(g->sphere, Vector3{0}, Vector3{0,0,1}, g->rotation, Vector3{1, 1, 1}, RAYWHITE);
     EndMode3D();
 
     DrawPlayerUI(p);
@@ -42,15 +66,18 @@ int main(void) {
     HideCursor();
     SetTargetFPS(1000);
 
-    Player *p = NewPlayer();
+    p = NewPlayer();
+    g = NewGlobe();
 
     while (!WindowShouldClose()) {
-        update(p);
-        draw(p);
+        update();
+        draw();
         DrawFPS(10, 10);
     }
 
+    CleanupGlobe(g);
     CleanupPlayer(p);
+
     CloseWindow();
 
     return 0;
